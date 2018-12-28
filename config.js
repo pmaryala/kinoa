a3cd457689db99178975be8a606ff7b16d03c855
adaps.priyanka.maryala <AIN009@adaps.com>
2018-12-28 11:23:51 +0530

    modified config file

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ exports.config = {
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-          args: ['no-sandbox', 'disable-gpu']
+          args: ['no-sandbox', 'disable-gpu', 'headless']
         }
       },
       ignoreUncaughtExceptions: true,
